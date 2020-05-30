ddcffcac419febc6f942c062a2380c0c8af77d74
James Bradford <40271160+MarbleToast@users.noreply.github.com>
2020-05-30 11:21:08 +0100

    Create project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css"
+
+import { Box } from "@material-ui/core"
+import CustomAreaGraph from "./components/CustomAreaGraph"
+import React from "react"
+
+function App() {
+	return (
+		<Box>
+			<a href="/hello">Hello</a>
+			<CustomAreaGraph />
+		</Box>
+	)
+}
+
+export default App
