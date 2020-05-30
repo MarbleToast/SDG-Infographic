b62b6953cbf6b081812860740dd302a1a7a9c50e
James Bradford <40271160+MarbleToast@users.noreply.github.com>
2020-05-30 15:30:44 +0100

    AAAAAAAAAAAAAAAAAAA

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,79 @@
 import "./App.css"
 
-import { Box } from "@material-ui/core"
-import CustomAreaGraph from "./components/CustomAreaGraph"
-import React from "react"
+import React, { useState } from "react"
+import ReactMapboxGl, { Feature, Layer } from "react-mapbox-gl"
 
-function App() {
+import { Box } from "@material-ui/core"
+import ParticulateDataMarch19 from "./datasets/MOP02J-20190401-L2V18.0.3.json"
+
+const App: React.FC<{}> = () => {
+	const [layerType, setLayerType] = useState("particulatesHeatmap")
+
+	const WorldMap = ReactMapboxGl({
+		accessToken:
+			"pk.eyJ1IjoibWFyYmxldG9hc3QiLCJhIjoiY2thdGwzbmZvMDJnMTJ5bzlzZ2N2dzRoeiJ9.CSGWtvwdfVBHb6YP0kurOw",
+	})
+
+	const layerPaint = {
+		"heatmap-weight": {
+			property: "COTotalColumn",
+			type: "exponential",
+			stops: [
+				[0, 0],
+				[5, 2],
+			],
+		},
+
+		"heatmap-intensity": {
+			stops: [
+				[0, 0],
+				[5, 1.2],
+			],
+		},
+		"heatmap-color": [
+			"interpolate",
+			["linear"],
+			["heatmap-density"],
+			0,
+			"rgba(33,102,172,0)",
+			0.25,
+			"rgb(103,169,207)",
+			0.5,
+			"rgb(209,229,240)",
+			0.8,
+			"rgb(253,219,199)",
+			1,
+			"rgb(239,138,98)",
+			2,
+			"rgb(178,24,43)",
+		],
+		// Adjust the heatmap radius by zoom level
+		"heatmap-radius": {
+			stops: [
+				[0, 1],
+				[5, 50],
+			],
+		},
+	}
+
 	return (
 		<Box>
-			<a href="/hello">Hello</a>
-			<CustomAreaGraph />
+			<WorldMap
+				/*eslint-disable-next-line react/style-prop-object*/
+				style="mapbox://styles/mapbox/streets-v9"
+				containerStyle={{
+					height: "100vh",
+					width: "100vw",
+				}}
+			>
+				<Layer layerPaint={layerPaint}>
+					{(ParticulateDataMarch19 as any).features.map(
+						(el: any, index: number) => (
+							<Feature key={index} coordinates={el.latlng} properties={el} />
+						)
+					)}
+				</Layer>
+			</WorldMap>
 		</Box>
 	)
 }
--- src/components/CustomAreaGraph.tsx
+++ src/components/CustomAreaGraph.tsx
@@ -1,85 +1,18 @@
-import "../datasets/data.json"
-
 import { Area, AreaChart, CartesianGrid, Tooltip, XAxis, YAxis } from "recharts"
 
 import { Box } from "@material-ui/core"
 import React from "react"
 
-const data = [
-	{
-		name: "Page A",
-		uv: 1000,
-		pv: 2400,
-		amt: 2400,
-	},
-	{
-		name: "Page B",
-		uv: 3000,
-		pv: 1398,
-		amt: 2210,
-	},
-	{
-		name: "Page C",
-		uv: 2000,
-		pv: 9800,
-		amt: 2290,
-	},
-	{
-		name: "Page D",
-		uv: 2780,
-		pv: 3908,
-		amt: 2000,
-	},
-	{
-		name: "Page E",
-		uv: 1890,
-		pv: 4800,
-		amt: 2181,
-	},
-	{
-		name: "Page F",
-		uv: 2390,
-		pv: 3800,
-		amt: 2500,
-	},
-	{
-		name: "Page G",
-		uv: 3490,
-		pv: 4300,
-		amt: 2100,
-	},
-]
-
 export default class CustomAreaGraph extends React.Component {
-	state = {
-		areaKey: "uv",
-		update: 0,
-	}
 	render() {
 		return (
 			<Box>
-				<AreaChart
-					width={800}
-					height={400}
-					data={data}
-					onClick={() => {
-						this.setState({
-							areaKey: this.state.areaKey === "uv" ? "pv" : "uv",
-							update: this.state.update + 1,
-						})
-					}}
-					key={this.state.update}
-				>
+				<AreaChart width={800} height={400}>
 					<CartesianGrid strokeDasharray="3 3" />
 					<XAxis dataKey="name" />
 					<YAxis />
 					<Tooltip />
-					<Area
-						type="monotone"
-						dataKey={this.state.areaKey}
-						stroke="#eee"
-						fill="#aaa4d8"
-					/>
+					<Area type="monotone" dataKey="" stroke="#eee" fill="#aaa4d8" />
 				</AreaChart>
 			</Box>
 		)
